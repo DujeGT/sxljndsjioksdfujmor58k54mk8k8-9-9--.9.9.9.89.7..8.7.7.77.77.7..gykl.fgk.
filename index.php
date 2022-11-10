ce5e784866b675e03bc7fe043e7da9d7ab1ecb3e
DujeGT <105517119+DujeGT@users.noreply.github.com>
2022-11-10 15:07:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include ("anti_ddos/start.php"); //write this at the top of your PHP application and all is done!!!
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Example Web page protected!
+	</title>
+</head>
+	<body>
+		...
+		<h2>Example Web page protected!</h2>
+		...
+	</body>
+</html>
